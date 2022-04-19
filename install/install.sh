ceb56c7c16f16fbcf09aac7d7ac10e44b423a02a
Thomas Cram <tcram@ucar.edu>
2022-04-19 15:22:13 -0600

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -3,61 +3,37 @@
 #  This script will make bufrsurface.x which to extract data from ADP BUFR
 #  input files, and place the data into a basic text file.  It is used to
 #  extract data from these kinds of files:
-#      gdas.adpupa.tHHz.YYYYMMDD.bufr 
-#      gdas.aircft.tHHz.YYYYMMDD.bufr
-#      gdas.satwnd.tHHz.YYYYMMDD.bufr 
-#      gdas.aircar.tHHz.YYYYMMDD.bufr
+#      gdas.adpsfc.tHHz.YYYYMMDD.bufr 
+#      gdas.sfcshp.tHHz.YYYYMMDD.bufr
 #
 #  dumpbufr.x:        used to dump all contents of a BUFR file.
-#  ** Make sure the "ar" command location has been set in your path
-#  environment variable.  Type "which ar" to check if this is done. **
+#
+#  Note: NCEP BUFRLIB library is required to compile this source code,
+#        and the location of your local installation of BUFRLIB should
+#        be defined in the 'LIB' variable below.  The NCEP BUFRLIB 
+#        software is available at
+#        https://emc.ncep.noaa.gov/emc/pages/infrastructure/bufrlib.php
 #  ------------------------------------------------------------------------
  
 set -eua
  
-#  ------------------------------------------------------------------------
-#  CPLAT - platform type (linux,sgi,aix,sun)
-#  ------------------------------------------------------------------------
- 
-CPLAT=linux
 SRC=../src
-LIB=../lib
+LIB=/path/to/bufrlib.a
 EXE=../exe
 INSTALL=.
 
-#  different platforms use different link name protocols
-#  -----------------------------------------------------
-
-# if using linux, BUFR files must be run through the "grabbufr/grabbufr.sh" script
-# with the resulting output used as input for the decoders.  Set appropriate compiler
-# in grabbufr.sh, and exe/convert.csh
- 
 cflag=""
 fflag=""
 
-if [ $CPLAT = linux ]
-then
-   export FC=gfortran
-   export CC=gcc
-   fflag=" -DUNDERSCORE -fno-second-underscore -w"
-   cflag=" -DUNDERSCORE -w"
-fi
-
-#  Compile and archive the Bufr Library
-#  ------------------------------------
-echo "Compiling BUFRLIB Library..."
-cd $LIB
-if [ -e bufrlib.a ]
-then
-  rm bufrlib.a
-fi
-$LIB/makebufrlib.sh
-cd $INSTALL
+export FC=gfortran
+export CC=gcc
+fflag=" -DUNDERSCORE -fno-second-underscore -w"
+cflag=" -DUNDERSCORE -w"
 
 #  Compile the decode programs
 #  ---------------------------------------
  
-echo "Compiling bufr_configdecode_ADPupa programs..."
+echo "Compiling bufr_configdecode_ADPsfc programs..."
 $FC $fflag -c $SRC/dumpbufr.f
 $FC $fflag -c $SRC/bufrsurface.f
  
@@ -65,8 +41,8 @@ $FC $fflag -c $SRC/bufrsurface.f
 #  -----------------------------
 
 echo "Linking..."
-$FC $fflag -o $EXE/dumpbufr.x dumpbufr.o $LIB/bufrlib.a
-$FC $fflag -o $EXE/bufrsurface.x bufrsurface.o $LIB/bufrlib.a
+$FC $fflag -o $EXE/dumpbufr.x dumpbufr.o $LIB
+$FC $fflag -o $EXE/bufrsurface.x bufrsurface.o $LIB
 
 #  clean up
 #  --------
